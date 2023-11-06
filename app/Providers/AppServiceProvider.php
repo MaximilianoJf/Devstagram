0ac608cca678b336223ef6ab605e93df384b8cb7
Maximilianojf <majf2704@gmail.com>
2023-11-05 23:01:54 -0300

    AppserviceProvieder con httsps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);
+        if($this->app->environment('production')){
+            URL::forceScheme('https');
+        }
     }
 }
